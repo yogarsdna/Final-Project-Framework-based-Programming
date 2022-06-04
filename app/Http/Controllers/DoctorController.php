01d02520353e0df52cd57f8ea12755452b395d61
Rafi Akbar <rafiakbarrafsanjani51@gmail.com>
2022-06-04 22:58:20 +0700

    Update DoctorController.php

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\User;
 class DoctorController extends Controller
 {
     /**
@@ -13,7 +13,9 @@ class DoctorController extends Controller
      */
     public function index()
     {
-        //
+    
+        $users  = User::where('role_id','!=',3)->get();
+        return view('admin.doctor.index',compact('users'));
     }
 
     /**
@@ -34,7 +36,18 @@ class DoctorController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validateStore($request);
+        $data = $request->all();
+        $name = (new User)->userAvatar($request);
+
+        $data['image'] = $name;
+        $data['password'] = bcrypt($request->password);
+        User::create($data);
+
+        return redirect()->back()->with('message','Doctor added successfully');
+
+
+        
     }
 
     /**
@@ -45,7 +58,8 @@ class DoctorController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::find($id);
+        return view('admin.doctor.delete',compact('user'));
     }
 
     /**
@@ -56,7 +70,8 @@ class DoctorController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::find($id);
+        return view('admin.doctor.edit',compact('user'));
     }
 
     /**
@@ -68,7 +83,24 @@ class DoctorController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validateUpdate($request,$id);
+        $data = $request->all();
+        $user = User::find($id);
+        $imageName = $user->image;
+        $userPassword = $user->password;
+        if($request->hasFile('image')){
+            $imageName =(new User)->userAvatar($request);
+            unlink(public_path('images/'.$user->image));
+        }
+        $data['image'] = $imageName;
+        if($request->password){
+            $data['password'] = bcrypt($request->password);
+        }else{
+            $data['password'] = $userPassword;
+        }
+         $user->update($data);
+        return redirect()->route('doctor.index')->with('message','Doctor updated successfully');
+
     }
 
     /**
@@ -79,6 +111,52 @@ class DoctorController extends Controller
      */
     public function destroy($id)
     {
-        //
+       if(auth()->user()->id == $id){
+            abort(401);
+       }
+       $user = User::find($id);
+       $userDelete = $user->delete();
+       if($userDelete){
+        unlink(public_path('images/'.$user->image));
+       }
+        return redirect()->route('doctor.index')->with('message','Doctor deleted successfully');
+
     }
+
+    public function validateStore($request){
+        return  $this->validate($request,[
+            'name'=>'required',
+            'email'=>'required|unique:users',
+            'password'=>'required|min:6|max:25',
+            'gender'=>'required',
+            'education'=>'required',
+            'address'=>'required',
+            'department'=>'required',
+            'phone_number'=>'required|numeric',
+            'image'=>'required|mimes:jpeg,jpg,png',
+            'role_id'=>'required',
+            'description'=>'required'
+
+       ]);
+    }
+    public function validateUpdate($request,$id){
+        return  $this->validate($request,[
+            'name'=>'required',
+            'email'=>'required|unique:users,email,'.$id,
+          
+            'gender'=>'required',
+            'education'=>'required',
+            'address'=>'required',
+            'department'=>'required',
+            'phone_number'=>'required|numeric',
+            'image'=>'mimes:jpeg,jpg,png',
+            'role_id'=>'required',
+            'description'=>'required'
+
+       ]);
+    }
+   
+
+
+ 
 }
