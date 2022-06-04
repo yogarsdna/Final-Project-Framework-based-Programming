924a47e4539066ac8cf8773d4ecd16de91b5e2d7
Rafi Akbar <rafiakbarrafsanjani51@gmail.com>
2022-06-05 00:40:18 +0700

    Create PatientlistController.php

---
+++ app/Http/Controllers/PatientlistController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Booking;
+class PatientlistController extends Controller
+{
+    public function index(Request $request)
+    {
+    	date_default_timezone_set('Australia/Melbourne');
+    	if($request->date){
+    		$bookings = Booking::latest()->where('date',$request->date)->get();
+    		return view('admin.patientlist.index',compact('bookings'));
+    	}
+    	$bookings = Booking::latest()->where('date',date('Y-m-d'))->get();
+    	return view('admin.patientlist.index',compact('bookings'));
+    }
+
+    public function toggleStatus($id)
+    {
+        $booking  = Booking::find($id);
+        $booking->status =! $booking->status;
+        $booking->save();
+        return redirect()->back();
+
+    }
+
+    public function allTimeAppointment()
+    {
+        $bookings = Booking::latest()->paginate(20);
+        return view('admin.patientlist.index',compact('bookings'));
+    }
+
+  
+}
