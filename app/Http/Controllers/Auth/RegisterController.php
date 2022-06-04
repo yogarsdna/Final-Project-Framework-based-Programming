0ba4e97a05e7f228db8591f2f3af6bbfdc18584f
Rafi Akbar <rafiakbarrafsanjani51@gmail.com>
2022-06-05 00:37:07 +0700

    Add some files

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,12 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use App\Models\User;
+use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use App\Models\Role;
-
+use App\Role;
 class RegisterController extends Controller
 {
     /*
@@ -54,7 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'gender' => 'required'
+            'gender'=>'required'
         ]);
     }
 
@@ -62,17 +61,17 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\Models\User
+     * @return \App\User
      */
-    protected function create(array $data)
-    {
-        $role = Role::where('name', 'patient')->first();
+    protected function create(array $data){
+        $role = Role::where('name','patient')->first();
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'role_id' => $role->id,
-            'gender' => $data['gender']
+            'role_id'=>$role->id,
+            'gender'=>$data['gender'],
+            
         ]);
     }
 }
