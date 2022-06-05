1e4eb769c4952e4f03f48ed54affbfdb2c9713a3
hashfiputrazah <74090466+hashfiputrazah@users.noreply.github.com>
2022-06-05 21:17:42 +0700

    Add files via upload
    vendor

---
+++ vendor/acaronlex/laravel-calendar/src/facades/Calendar.php
@@ -0,0 +1,12 @@
+<?php namespace Acaronlex\LaravelCalendar\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Calendar extends Facade
+{
+
+    protected static function getFacadeAccessor()
+    {
+        return 'laravel-calendar';
+    }
+}
