f0eaffedae05c136db4eaf44cf068cfb02240922
hashfiputrazah <74090466+hashfiputrazah@users.noreply.github.com>
2022-06-05 21:15:11 +0700

    Delete acaronlex/laravel-calendar directory

--- acaronlex/laravel-calendar/src/views/script.blade.php
+++
@@ -1,9 +0,0 @@
-<script>
-    $(document).ready(function(){
-        var calendarEl = document.getElementById('calendar-{{ $id }}');
-
-        var calendar = new FullCalendar.Calendar(calendarEl, {!! $options !!});
-
-        calendar.render();
-    });
-</script>
