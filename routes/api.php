c2e213764fb5ebd8bf0059492d087db957f84c18
yogarsdna <gedeyoga54@gmail.com>
2022-06-05 17:02:42 +0700

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/doctors/today','FrontendController@doctorToday');
+Route::post('/finddoctors','FrontendController@findDoctors');
+
+
--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,66 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/event','EventCalender@index');
+
+Route::get('/','FrontendController@index');
+
+
+Route::get('/new-appointment/{doctorId}/{date}','FrontendController@show')->name('create.appointment');
+
+
+
+Route::group(['middleware'=>['auth','patient']],function(){
+
+	Route::post('/book/appointment','FrontendController@store')->name('booking.appointment');
+
+	Route::get('/my-booking','FrontendController@myBookings')->name('my.booking');
+
+	Route::get('/user-profile','ProfileController@index');
+	Route::post('/user-profile','ProfileController@store')->name('profile.store');
+	Route::post('/profile-pic','ProfileController@profilePic')->name('profile.pic');
+	Route::get('/my-prescription','FrontendController@myPrescription')->name('my.prescription');
+
+
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
 
+Route::get('/dashboard','DashboardController@index');
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('doctor', 'DoctorController');
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+Route::group(['middleware'=>['auth','admin']],function(){
+	Route::resource('doctor','DoctorController');
+	Route::get('/patients','PatientlistController@index')->name('patient');
+	Route::get('/patients/all','PatientlistController@allTimeAppointment')->name('all.appointments');
+	Route::get('/status/update/{id}','PatientlistController@toggleStatus')->name('update.status');
+	Route::resource('department','DepartmentController');
+
+
+});
+
+Route::group(['middleware'=>['auth','doctor']],function(){
+
+	Route::resource('appointment','AppointmentController');
+	Route::post('/appointment/check','AppointmentController@check')->name('appointment.check');
+	Route::post('/appointment/update','AppointmentController@updateTime')->name('update');
+
+	Route::get('patient-today','PrescriptionController@index')->name('patients.today');
+
+	Route::post('/prescription','PrescriptionController@store')->name('prescription');
+
+	Route::get('/prescription/{userId}/{date}','PrescriptionController@show')->name('prescription.show');
+	Route::get('/prescribed-patients','PrescriptionController@patientsFromPrescription')->name('prescribed.patients');
+
+
+});
+
+// Route::post('/api/doctors','FrontendController@getDoctors');
+// Route::get('/api/doctors/today','FrontendController@doctorToday');
+
+
+
